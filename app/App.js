7fbb9dfe0342351e3fb18499b631e186c49be9ab
Stephanie <libby3steph@gmail.com>
2020-04-25 08:10:54 +0100

    local notification triggered when app is in background or foreground

--- app/App.js
+++ app/App.js
@@ -1,9 +1,40 @@
+
+
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {Component} from 'react';
 import {YellowBox} from 'react-native';
 import AuthNavigator from './routes/Authentication';
+import LocalPushController from 'C:/Users/steph/vscodeprojects/devba/app/services/LocalPushController.js';
+import {View} from 'react-native';
+import Firebase from 'firebase';
+import 'firebase/database';
+import PushNotification from 'react-native-push-notification';
 
-export default function devba() {
-  YellowBox.ignoreWarnings(['Setting a timer']);
-  return <AuthNavigator />;
-}
+
+export default class devba extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      postList: [],
+    };
+  }
+  
+
+  newPostNotification = () => {
+      const ref = Firebase.database().ref('/posts');
+      
+    ref.limitToLast(1).on('child_added', function(childSnapshot, prevChildName) {  
+      console.log("I hear a new post!")
+      LocalPushController()   
+   });
+    };
+
+    
+  render() {
+    this.newPostNotification();
+    return ( 
+        <AuthNavigator /> 
+    
+      )
+}};
