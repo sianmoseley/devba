31ff516f3ee3609427a236c8c38084b8abf114fc
Matthew Goostrey <goostreymatthew@gmail.com>
2020-05-01 15:41:42 +0100

    tidy up notifcation

--- app/App.js
+++ app/App.js
@@ -1,41 +1,29 @@
-
-
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {YellowBox} from 'react-native';
 import AuthNavigator from './routes/Authentication';
-import LocalPushController from '../services/LocalPushController.js';
-import {View} from 'react-native';
+import LocalPushController from './services/LocalPushController.js';
 import Firebase from 'firebase';
 import 'firebase/database';
-import PushNotification from 'react-native-push-notification';
-
-
-// Firebase.initializeApp(firebaseConfig);
 
 export default class devba extends Component {
-
-   
-
   newPostNotification = () => {
-      const ref = Firebase.database().ref('/posts');
-      
-    ref.limitToLast(1).on('child_added', function(childSnapshot) {  
-      console.log("I hear a new post!")
-      console.log(childSnapshot + ' is the new post')
-      LocalPushController()   
-   });
-    };
+    const ref = Firebase.database().ref('/posts');
 
-    componentDidMount() {
-      this.newPostNotification();
-    }
-    
+    ref.limitToLast(1).on('child_added', function(childSnapshot) {
+      console.log('I hear a new post!');
+      console.log(childSnapshot + ' is the new post');
+      LocalPushController();
+    });
+  };
+
+  componentDidMount() {
+    this.newPostNotification();
+  }
+
   render() {
     //plugs into index.js
     YellowBox.ignoreWarnings(['Setting a timer']);
-    return ( 
-        <AuthNavigator /> 
-    
-      )
-}};
+    return <AuthNavigator />;
+  }
+}
--- app/services/LocalPushController.js
+++ app/services/LocalPushController.js
@@ -1,5 +1,5 @@
 import PushNotification from 'react-native-push-notification';
- 
+
 PushNotification.configure({
   // (required) Called when a remote or local notification is opened or received
   onNotification: function(notification) {
@@ -8,12 +8,11 @@ PushNotification.configure({
   popInitialNotification: true,
   requestPermissions: true,
 });
- 
+
 export default function LocalNotification() {
   PushNotification.localNotification({
     autoCancel: true,
-    bigText:
-      'Someone has added a new post to Big APPetite!',
+    bigText: 'Someone has added a new post to Big APPetite!',
     // subText: 'Local Notification Demo',
     title: 'New post in Big APPetite',
     message: 'put the details of the post here',
