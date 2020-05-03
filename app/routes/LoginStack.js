4b548e6c0b0b1358f2c5f0d289a56d4ed7c39b4d
Stephanie <libby3steph@gmail.com>
2020-05-03 11:44:13 +0100

    notifications code moved to login stack
    code refactored for functional component
    notifications still fire when user is logged out

--- app/routes/LoginStack.js
+++ app/routes/LoginStack.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import RootStack from './RootStack';
+import LocalPushController from '../services/LocalPushController';
+import Firebase from 'firebase';
 
 //directs to RootStack
 
 const Stack = createStackNavigator();
 
 export default function LogInStack() {
+
+  useEffect(() => {
+    const ref = Firebase.database().ref('/posts');
+    ref.limitToLast(1).on('child_added', function(childSnapshot) {
+      console.log('I hear a new post!');
+      console.log(childSnapshot + ' is the new post');
+      LocalPushController();
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator headerMode="none">
@@ -16,3 +28,4 @@ export default function LogInStack() {
     </NavigationContainer>
   );
 }
+
