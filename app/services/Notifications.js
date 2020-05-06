ff658e8ec176a48582cdfa443c49653b743f83c2
Stephanie <libby3steph@gmail.com>
2020-05-06 13:51:42 +0100

    logic for notifications based on preferences correct but need to read preferences from db

--- app/services/Notifications.js
+++ app/services/Notifications.js
@@ -6,26 +6,33 @@ import LocalPushController from '../services/LocalPushController';
 
 
 export default function Notifications() {
+    let user = Firebase.auth().currentUser;
+    let uid = user.uid;
+    let dbuser = Firebase.database().ref('users/' + uid);
+    //need to change this so it's actually reading user preferences from the db
+    let notificationPreferences = true;
 
     useEffect(() => {
-        let first = true;
-        const ref = Firebase.database().ref('/posts');
-        const onValueChange = function(snapshot, prevChildKey) {
-            if (first) {
-                first = false;
-            } else {
-                let newPost = snapshot.val();
-                console.log("Heading: " + newPost.heading);
-                console.log("Description: " + newPost.description);
-                console.log("Location: " + newPost.location);
-                LocalPushController(newPost.heading, newPost.description, newPost.location);    
-            }};  
 
-        ref.limitToLast(1).on("child_added", onValueChange);
+        if (notificationPreferences === true) {
+            let first = true;
+            const ref = Firebase.database().ref('/posts');
+            const onValueChange = function(snapshot, prevChildKey) {
+                if (first) {
+                    first = false;
+                } else {
+                    let newPost = snapshot.val();
+                    console.log("Heading: " + newPost.heading);
+                    console.log("Description: " + newPost.description);
+                    console.log("Location: " + newPost.location);
+                    LocalPushController(newPost.heading, newPost.description, newPost.location);    
+                }};  
 
-        return function cleanup() {
-            ref.off("child_added", onValueChange);
-        };
+            ref.limitToLast(1).on("child_added", onValueChange);
+
+            return function cleanup() {
+                ref.off("child_added", onValueChange);
+        };}
 });    
 
       return (null);
