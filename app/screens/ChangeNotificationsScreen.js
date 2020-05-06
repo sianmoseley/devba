ab322c7c207562660f67f7b7ae64361b1ba67185
Stephanie <libby3steph@gmail.com>
2020-05-06 13:08:45 +0100

    sorted out notifications setting in database
    notifications attribute added along with other details when you Register
    notifications attribute can be changed by the user
    not yet sorted it out for existing users
    currently no logic for firing notifications or otherwise depending on user preferences

--- app/screens/ChangeNotificationsScreen.js
+++ app/screens/ChangeNotificationsScreen.js
@@ -20,10 +20,17 @@ import {AuthInput, AuthSwitch} from '../config/Variables';
 
 
 export default function ChangeNotificationsScreen({navigation}) {
-  //logic to change notification settings to go here
-  
+    let user = Firebase.auth().currentUser;
+    let uid = user.uid;
+    let currentUsername = user.displayName;
 
-function ChangeNotifications() {
+function ChangeNotifications(uid, yesno) {
+    console.log('uid: ', uid, 'notifications preference: ', yesno);
+    //update notification preferences in the db
+    Firebase.database().ref('users/' + uid).update({
+        notifications: yesno,
+      });
+    //alert to confirm to user that their notification settings have changed
     Alert.alert('Notification Settings', 'Your notification settings have been changed.', [
         {
           text: 'OK',
@@ -38,11 +45,11 @@ function ChangeNotifications() {
             Would you like to receive notifications when there are new posts?
         </Text>
         <Button
-            onPress={() => ChangeNotifications()}
+            onPress={() => ChangeNotifications(uid, true)}
             title="Yes"
         />
         <Button
-            onPress={() => ChangeNotifications()}
+            onPress={() => ChangeNotifications(uid, false)}
             title="No"
         />
       </View>
