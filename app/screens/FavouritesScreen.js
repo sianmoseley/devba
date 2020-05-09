cb5d52a1f9e36875dc7b2e8afac6f2088be81c04
Matthew Goostrey <goostreymatthew@gmail.com>
2020-05-09 16:49:34 +0100

    altered search and fav

--- app/screens/FavouritesScreen.js
+++ app/screens/FavouritesScreen.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Image, FlatList, Text, View} from 'react-native';
+import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
+import {Icon} from 'react-native-elements';
 import Firebase from 'firebase';
 import {globalStyles} from '../config/Styles';
 import {Post} from '../config/Variables';
@@ -13,6 +14,7 @@ export default class FavouritesScreen extends Component {
     this.state = {
       //set value of favList as empty array
       favList: [],
+      likedPosts: [],
     };
   }
 
@@ -41,6 +43,32 @@ export default class FavouritesScreen extends Component {
         this.setState({favList: favArray});
       }
     });
+
+    //current user unique id
+    const userID = Firebase.auth().currentUser.uid;
+    Firebase.database()
+      //look at current users liked posts table
+      .ref('favourites/' + userID)
+      .on('value', snapshot => {
+        //store value of current users liked posts
+        const likedObject = snapshot.val();
+        if (!likedObject) {
+          console.log('USER HAS NO LIKES:', Date(Date.now()));
+          //solves bug if user unlikes post from fav screen
+          //but heart of last post to be unliked is still filled in
+          this.setState({
+            likedPosts: [],
+          });
+        } else {
+          console.log('LIKES RETRIEVED:', Date(Date.now()));
+          //stores id of ALL posts user has liked
+          let postID = Object.keys(likedObject);
+          //set likedPosts array to above postID value
+          this.setState({
+            likedPosts: postID,
+          });
+        }
+      });
   };
 
   render() {
@@ -65,25 +93,92 @@ export default class FavouritesScreen extends Component {
           <FlatList
             keyExtractor={post => post.id}
             //inserts data to be rendered as the array of favourite posts
-            data={this.state.favList.sort(a => a.createdAt.localeCompare())}
+            // data={this.state.favList.sort(a => a.createdAt.localeCompare())}
+            data={this.state.favList}
             //each item in the array is identified as 'post'
             renderItem={({item: post}) => (
-              <Post
-                //specified Post component rendered as item in list with fields from database rendered in their designated sections
-                key={post.id}
-                heading={post.heading}
-                description={post.description}
-                location={post.location}
-                createdAt={post.createdAt}
-                createdBy={post.createdBy}
-                uri={{uri: post.uri}}
-                onPress={() => {
-                  const postKey = post.id;
-                  Firebase.database()
-                    .ref('favourites/' + userKey + '/' + postKey)
-                    .remove();
-                }}
-              />
+              <TouchableOpacity
+                onPress={() =>
+                  this.props.navigation.navigate('LocatePostScreen', {
+                    location: post.location,
+                  })
+                }>
+                <View style={globalStyles.postContainer}>
+                  <Text style={globalStyles.postText}>
+                    {post.heading}
+                    {'\n'}@{' '}
+                    <Text style={{fontWeight: 'bold'}}>{post.location}</Text>
+                    {'\n'}
+                    {post.description}
+                    {'\n'}
+                    listed by{' '}
+                    <Text style={{fontWeight: 'bold'}}>{post.createdBy}</Text>
+                    {'\n'}
+                    on{' '}
+                    <Text style={{fontWeight: 'bold'}}>{post.createdAt}</Text>
+                  </Text>
+
+                  {/* SIAN - IMAGE INSERTED INTO POST VIEW, HAPPY FOR THIS TO BE MOVED, SIZE CHANGED ETC */}
+
+                  <Image
+                    style={{alignSelf: 'center', height: 200, width: 200}}
+                    source={{uri: post.url}}
+                  />
+
+                  <View style={globalStyles.iconMargin}>
+                    <Icon
+                      raised
+                      iconStyle={globalStyles.icon}
+                      name={
+                        this.state.likedPosts.indexOf(post.id) > -1
+                          ? 'heart'
+                          : 'heart-o'
+                      }
+                      size={28}
+                      type="font-awesome"
+                      onPress={() => {
+                        const userKey = Firebase.auth().currentUser.uid;
+                        const postKey = post.id;
+                        const favRef = Firebase.database().ref(
+                          'favourites/' + userKey + '/' + postKey,
+                        );
+
+                        //check that the array doesn't contain the post id (i.e. the post was not previously liked)
+                        if (this.state.likedPosts.indexOf(post.id) === -1) {
+                          favRef.set({
+                            id: postKey,
+                            heading: post.heading,
+                            description: post.description,
+                            location: post.location,
+                            createdAt: post.createdAt,
+                            createdBy: post.createdBy,
+                          });
+                        } else {
+                          favRef.remove();
+                          //grab index of post.id
+                          let index = this.state.likedPosts.indexOf(post.id);
+                          //splice the post.id from the rendered likedPosts array
+                          this.state.likedPosts.splice(index, 1);
+                          //then set state for rendered likedPosts to post-splice likedPosts
+                          this.setState({
+                            likedPosts: this.state.likedPosts,
+                          });
+                        }
+                      }}
+                    />
+                    <Icon
+                      raised
+                      iconStyle={globalStyles.icon}
+                      name="flag-o"
+                      size={28}
+                      type="font-awesome"
+                      onPress={() =>
+                        this.props.navigation.navigate('ReportPostScreen', post)
+                      }
+                    />
+                  </View>
+                </View>
+              </TouchableOpacity>
             )}
           />
         )}
